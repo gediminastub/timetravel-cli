a45f846505ca393b210f85a3682c15bb8d3ca962
Gediminas Tubelevicius <gtubelevicius@gmail.com>
2023-12-05 14:34:31 +0200

    Init 2

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,21 @@
 import test from 'ava'
 
-import { orderPizza } from './index'
+import {orderPizza} from './index'
 
 test.before(async t => {
-  console.log('Starting tests!')
+    console.log('Starting tests!')
 })
 
 test('can order a pizza', async t => {
 
-  let result = await orderPizza({
-    peppers: false,
-    pineapple: false,
-    bbqSauce: false,
-    cheeseType: 'swiss'
-  })
+    let result = await orderPizza({
+        peppers: false,
+        pineapple: false,
+        bbqSauce: false,
+        cheeseType: 'swiss'
+    })
 
-  t.true(result.message.includes('you ordered a pizza'))
-  t.true(result.message.includes('swiss'))
+    t.true(result.message.includes('you ordered a pizza'))
+    t.true(result.message.includes('swiss'))
 
 })
